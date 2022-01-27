1cc36b89f6382582343d3aa9ed3ebfaefb2ea4b4
DarwinPon <li.xinlai@gmail.com>
2022-01-26 21:43:48 -0500

    Background theme now plays continuously

--- .idea/sound.py
+++ .idea/sound.py
@@ -18,7 +18,7 @@ class Sound():
         self.mixer.Sound.play(self.RANDOM_ANGLE_SOUND)
 
     def bgm(self):
-        self.mixer.Sound.play(self.BGM)
+        self.mixer.Sound.play(self.BGM,loops=-1)
 
     def normal_hit(self):
         self.mixer.Sound.play(self.NORMAL_HIT_SOUND)
