7f54d153eafbeb9c4dd0a00c0722961e258ad263
DarwinPon <li.xinlai@gmail.com>
2022-01-26 23:34:57 -0500

    Fixed multiple issues

--- start.py
+++ start.py
@@ -23,14 +23,14 @@ font = pygame.font.SysFont('Verdana', 45)
 # set RGB of colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
-BG = (248, 249, 216)# background yellow
+BG = (248, 249, 212)# background yellow
 
 #read and transform images needed
-start_ori = pygame.image.load(os.path.join('assets', 'startScreen.png'))
+start_ori = pygame.image.load(os.path.join('pictures', 'startScreen.png'))
 start_img = pygame.transform.scale(start_ori, (796,562))
-ball_ori = pygame.image.load(os.path.join('assets', 'ball_yellow.png')).convert()
+ball_ori = pygame.image.load(os.path.join('pictures', 'ball_yellowbg.png')).convert()
 ball_img = pygame.transform.scale(ball_ori, (200,200))
-ball_img.set_colorkey(BG)
+ball_img.set_colorkey(-1, pygame.RLEACCEL)
 
 
 def interface():
@@ -43,7 +43,7 @@ def interface():
     #displaying start image
     WIN.blit(start_img,(242,79))
     pygame.display.flip()
-    pygame.time.wait(300)
+    pygame.time.wait(3000)
 
     #fading out the start image
     fadeOut(1280, 720, BG)
@@ -74,11 +74,12 @@ def tutorial():
 def fadeOut(width, height, color):
     sur = pygame.Surface((width, height))
     sur.fill(color)
-    for alpha in range (90, 300):
-        sur.set_alpha(alpha)
+
+    for alpha in range (0, 3000):
+        sur.set_alpha(alpha//10)
         WIN.blit(sur, (0,0))
         pygame.display.flip()
-        pygame.time.wait(90)
+        # pygame.time.wait(90)
 
 #rotate the ball and blit the img with its new position
 def blitRotateBall(win, img):
@@ -92,21 +93,50 @@ def blitRotateBall(win, img):
         mx, my = pygame.mouse.get_pos()
         img_copy = pygame.transform.rotate(img, angle)
         img_copy.set_colorkey(BG)
-        win.blit(img_copy, (mx - int(img.get_width() / 2), my - int(img.get_height() / 2)))
+        win.blit(img_copy, (mx - int(img.get_width() / 2), my - int(img.get_height() / 2)),pygame.Rect(img.get_width()//2-30,img.get_height()//2-30,100,100))
         pygame.display.flip()
         pygame.time.wait(50)
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 loop = False
 
+def interface():
+    #start music
+    mixer.music.play()
+    WIN.fill(BG)
+    pygame.display.flip()
+    pygame.time.wait(100)
+
+    #displaying start image
+    WIN.blit(start_img,(242,79))
+    pygame.display.flip()
+    pygame.time.wait(3000)
+
+    #fading out the start image
+    fadeOut(1280, 720, BG)
+        
+    # blit the text onto the screen
+    start_text = font.render('Click to Start', True, BLACK)
+    WIN.blit(start_text, (540, 160))
+    pygame.display.flip()
+
+    #rotate and move ball image
+    blitRotateBall(WIN, ball_img)
+
+    #set up click to start game play and text disappearance
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            fadeOut(1280, 720, BLACK)
+            tutorial()
+
 
 def main():
     run = True
-    while run:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                pygame.quit()
-        interface()
+    # while run:
+        # for event in pygame.event.get():
+            # if event.type == pygame.QUIT:
+                # run = False
+                # pygame.quit()
+    interface()
 
 main()
