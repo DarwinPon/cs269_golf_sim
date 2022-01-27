14b0317f0650d256abb8e2aa1a2098bb200c4942
chloezhang2000 <47944883+chloezhang2000@users.noreply.github.com>
2022-01-26 21:56:06 -0500

    start.py in progress

--- start.py
+++ start.py
@@ -10,7 +10,7 @@ import os
 pygame.init()
 #load music
 mixer.init()
-mixer.music.load('../audios/BGM_start.mp3')
+mixer.music.load('audios/BGM_startingGame_LoveDream.mp3')
 WIDTH, HEIGHT = 1280, 720
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Golf Simulator")
@@ -18,7 +18,7 @@ clock = pygame.time.Clock()
 
 # game variables
 FPS = 30
-font = pygame.font.Font('freesansbold.ttf', 42)
+font = pygame.font.SysFont('Verdana', 45)
 
 # set RGB of colors
 WHITE = (255, 255, 255)
@@ -28,8 +28,8 @@ BG = (248, 249, 216)# background yellow
 #read and transform images needed
 start_ori = pygame.image.load(os.path.join('assets', 'startScreen.png'))
 start_img = pygame.transform.scale(start_ori, (796,562))
-ball_ori = pygame.image.load(os.path.join('assets', 'player1ball.png')).convert()
-ball_img = pygame.transform.scale(ball_ori, (60,60))
+ball_ori = pygame.image.load(os.path.join('assets', 'ball_yellow.png')).convert()
+ball_img = pygame.transform.scale(ball_ori, (200,200))
 ball_img.set_colorkey(BG)
 
 
@@ -38,12 +38,12 @@ def interface():
     mixer.music.play()
     WIN.fill(BG)
     pygame.display.flip()
-    pygame.time.wait(200)
+    pygame.time.wait(100)
 
     #displaying start image
     WIN.blit(start_img,(242,79))
     pygame.display.flip()
-    pygame.time.wait(200)
+    pygame.time.wait(300)
 
     #fading out the start image
     fadeOut(1280, 720, BG)
@@ -60,12 +60,21 @@ def interface():
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
             fadeOut(1280, 720, BLACK)
+            tutorial()
+
+
+def tutorial():
+    WIN.fill(BG)
+    pygame.display.flip()
+    pygame.time.wait(100)
+    tutorial_text = font.render('Hole-in-One is a 2D simulation game where two players compete by trying to launch their ball into a hole. Each player is represented as a golf ball, and their main objective is to traverse through a level filled with obstacles, and interactable terrains, and items in order to reach the goal.', True, BLACK)
+    WIN.blit(tutorial_text, (20, 160))
 
 #fade out function used to fade out a selected area
 def fadeOut(width, height, color):
     sur = pygame.Surface((width, height))
     sur.fill(color)
-    for alpha in range (0, 300):
+    for alpha in range (90, 300):
         sur.set_alpha(alpha)
         WIN.blit(sur, (0,0))
         pygame.display.flip()
@@ -99,4 +108,5 @@ def main():
                 run = False
                 pygame.quit()
         interface()
+
 main()
